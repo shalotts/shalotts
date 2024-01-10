691423284677c1c1e24299ac3bf8eb4e8eb0f352
shalotts <francyfox@yandex.ru>
2024-01-10 10:25:56 +0600

    fix: vite default config

--- app/shalotts.ts
+++ app/shalotts.ts
@@ -2,9 +2,10 @@ import { $shalotts, ENV_VAR } from '~/app/const.ts';
 import AppModule from '~/app/module/app/app.module.ts';
 import { defaultConfig, defineConfig, listen } from '~/app/module/config/config.ts';
 import type { IAppConfig, ITunnelOptions, ShalottsOptions } from '~/app/module/config/config.type.ts';
+import { defaultViteF3vConfig } from '~/app/module/config/config.vite.ts';
 import HttpModule from '~/app/module/http/http.module.ts';
 import type { PageContext } from '~/renderer/renderer.type.ts';
 
-export { HttpModule, AppModule, defineConfig, defaultConfig, listen, $shalotts, ENV_VAR };
+export { HttpModule, AppModule, defineConfig, defaultConfig, defaultViteF3vConfig, listen, $shalotts, ENV_VAR };
 
 export type { ITunnelOptions, ShalottsOptions, IAppConfig, PageContext };
