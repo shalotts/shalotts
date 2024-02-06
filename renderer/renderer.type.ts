c5b7adaab806004b4da8bb6781de41a7a655b27e
shalotts <francyfox@yandex.ru>
2024-02-06 14:27:17 +0600

    fix: build hook error

--- renderer/renderer.type.ts
+++ renderer/renderer.type.ts
@@ -1,7 +1,7 @@
 import { Component } from '~/app/module/vike/vike.type.ts';
 import { Config } from 'vike/types';
 import { Unhead } from '@unhead/schema';
-import { OnBeforeMountAppAsync, OnBeforeMountAppSync } from '~/renderer/+config.ts';
+import { vikeHook, vikeHookAsync } from '~/renderer/+config.ts';
 
 export type PageContext = {
   Page?: Component,
@@ -11,5 +11,6 @@ export type PageContext = {
 
 export type f3VRendererConfig = Config & {
   head: Unhead<object>
-  onBeforeMountApp?: OnBeforeMountAppSync | OnBeforeMountAppAsync;
+  onBeforeMountApp?: vikeHook | vikeHookAsync;
+  onCreateApp?: vikeHook | vikeHookAsync;
 }
