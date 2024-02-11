bf6c0db4d6798169fc7cc677094628948492719d
shalotts <francyfox@yandex.ru>
2024-02-11 20:58:08 +0600

    fix: tsup node compile 2

--- tsup.config.ts
+++ tsup.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
     'vue',
     'vue/server-renderer',
     'vike/server',
-  ]
-  // banner: {
-  //   js: `import{fileURLToPath}from"node:url";import path from"node:path";import{createRequire as topLevelCreateRequire}from"module";const require=topLevelCreateRequire(import.meta.url),__filename=fileURLToPath(import.meta.url),__dirname=path.dirname(__filename);`,
-  // },
+  ],
+  banner: {
+    js: `import{fileURLToPath}from"node:url";import path from"node:path";import{createRequire as topLevelCreateRequire}from"module";const require=topLevelCreateRequire(import.meta.url),__filename=fileURLToPath(import.meta.url),__dirname=path.dirname(__filename);`,
+  },
 });
